3f3f0e2c39f56d9bfba5fca9ce7e84d2c40ee7be
Hyun Bin Yim <hby0818@gmail.com>
2023-04-04 10:51:16 -0400

    finishedupdateepisode

--- levity/src/pages/EditEpisodeForm.jsx
+++ levity/src/pages/EditEpisodeForm.jsx
@@ -51,14 +51,14 @@ const getUpdate = async () => {
 }
 
   return localStorage.getItem('id') === "6428fdcacbf30d7c30089d83" && (
-    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className="isolate bg-[#e7e9ea] px-6 py-16 lg:px-8">
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">Episode Form</h2>
         <p className="mt-6 text-lg leading-8 text-gray-600">
           Use form to update this episode
         </p>
       </div>
-      <form onSubmit={handleSubmit} action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form onSubmit={handleSubmit} action="#" method="POST" className="mx-auto mt-8 max-w-xl">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div className="sm:col-span-2">
             <label htmlFor="title" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -151,10 +151,10 @@ const getUpdate = async () => {
             </div>
           </div>
         </div>
-        <div className="mt-10">
+        <div className="mt-10 flex justify-end border-t border-gray-900/10 pt-8">
           <button
             type="submit"
-            className="block w-full rounded-md bg-gray-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+            className="bg-[#e7e9ea] transform  border-2 w-28 h-28 rounded-full duration-500 border-black hover:bg-black hover:text-white text-2xl m-auto inset-0 font-semibold leading-7 text-center text-gray-800"
           >
             Update Episode
           </button>
